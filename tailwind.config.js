32831b5edc988e78046ba3472668a81947e5b7ea
Miko Nuñez <nunyezmiko@gmail.com>
2021-07-27 11:19:06 +0800

    added hedaer and custom fonts and colors to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'peach': '#FFD3BA',
+        'peach-dark': '#FF985C',
+      },
+      fontFamily: {
+        'roboto': ['Roboto'],
+      },
+    },
   },
   variants: {
     extend: {},
